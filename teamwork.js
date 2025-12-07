29d626953d6519d6e9be7c269df24057ab236bb2
unknown <nrilhamttp13@gmail.com>
2025-12-07 20:11:49 +0300

    Added By Nafis

--- teamwork.js
+++ teamwork.js
@@ -4,3 +4,31 @@ const teamMembers = ['Rafi', 'Meem', 'Shuvo'];
 let rafiScore = 0;
 let meemScore = 0;
 let shuvoScore = 0;
+
+const Rafi = {
+  id: Symbol('Rafi'),
+  name: 'Rafi',
+  age: 23,
+  isActive: true,
+  bugs: ['UI Bug', 'API Bug', 'Login Bug'],
+  bugCount: 3
+};
+
+const Meem = {
+  id: Symbol('Meem'),
+  name: 'Meem',
+  age: 21,
+  isActive: true,
+  bugs: ['UI Bug'],
+  bugCount: 1
+};
+
+const Shuvo = {
+  id: Symbol('Shuvo'),
+  name: 'Shuvo',
+  age: 25,
+  isActive: true,
+  bugs: ['UI Bug', 'API Bug', 'Logic Bug', 'DB Bug', 'Crash Bug', 'Auth Bug'],
+  bugCount: 6
+};
+
